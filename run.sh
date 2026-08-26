344f2a7175f4b980ff3572dcdf49e5c3f0459e8b
moslemi <mhmoslemi2338@gmail.com>
2026-08-26 18:30:29 -0400

    ad

--- run.sh
+++ run.sh
@@ -175,13 +175,27 @@ done
  
 
 
+# if [ "$resume_run" -eq 1 ]; then
+#     CUDA_VISIBLE_DEVICES=0,1,2,3,4 python train_multy.py "$@"
+# else
+#     CUDA_VISIBLE_DEVICES=0,1,2,3,4 python train_multy.py --problem erdos --config configs/erdos.yaml \
+#         --num-steps 22 --deterministic --seed 42 \
+#         --groups-per-step 5 --group-size 16 \
+#         --growth-force-step 3 --gpu-ids 0,1,2,3,4 \
+#         --growth-valid-yield 0.7 --growth-distinct-min 2 --growth-factor 2.0 --memory --feedback \
+#         --model-name /mnt/storage/mohammad/models/Qwen3-8B "$@"
+# fi
+
+
 if [ "$resume_run" -eq 1 ]; then
-    CUDA_VISIBLE_DEVICES=0,1,2,3,4 python train_multy.py "$@"
+    CUDA_VISIBLE_DEVICES=0,1,2,3 python train_multy.py \
+        --backend vllm --gpu-ids 1,2,3 "$@"
 else
-    CUDA_VISIBLE_DEVICES=0,1,2,3,4 python train_multy.py --problem erdos --config configs/erdos.yaml \
+    CUDA_VISIBLE_DEVICES=0,1,2,3 python train_multy.py --problem erdos --config configs/erdos.yaml \
+        --backend vllm \
         --num-steps 22 --deterministic --seed 42 \
-        --groups-per-step 5 --group-size 16 \
-        --growth-force-step 3 --gpu-ids 0,1,2,3,4 \
+        --groups-per-step 8 --group-size 64 \
+        --growth-force-step 3 --gpu-ids 1,2,3 \
         --growth-valid-yield 0.7 --growth-distinct-min 2 --growth-factor 2.0 --memory --feedback \
         --model-name /mnt/storage/mohammad/models/Qwen3-8B "$@"
 fi
