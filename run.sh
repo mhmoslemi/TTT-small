b90c58442dd24a5e5e3ae206692eeb342bde58de
moslemi <mhmoslemi2338@gmail.com>
2026-09-09 15:42:30 -0400

    d

--- run.sh
+++ run.sh
@@ -87,4 +87,5 @@ case "$memory_version" in
 esac
 
 # exec python3 train_multy.py --config "$config_path" "$@"
-exec python3 train_multy_CVaR.py --config "$config_path" --backend hf "$@" --advantage-mode rank
+# exec python3 train_multy_CVaR.py --config "$config_path" --backend hf "$@" --advantage-mode rank
+exec python3 train_multy_CVaR.py --config "$config_path" --backend hf "$@" --advantage-mode rank --fast --rank-clip-epsilon-low 0.2 --rank-clip-epsilon-high 0.35 --isolate-eval
